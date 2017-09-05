f7900c83e7a707ad9aa263f39c8359d6a0e5d107
mateogolf <rodriguezml11@mail.wlu.edu>
2017-09-05 14:08:18 -0400

    Notes on Inheritance in java

---
+++ Inheritance/Human.java
@@ -0,0 +1,13 @@
+class Human extends Mammal {
+    
+    public void goToWork(){
+        System.out.println("I'm going to work, something only humans can do.");
+    }
+    public void startSleeping() {
+        System.out.println("Toss and turn");
+        super.startSleeping();
+    }
+    public static void testStatic() {
+        System.out.println("The static method in Human");
+    }
+}
---
+++ Inheritance/Mammal.java
@@ -0,0 +1,18 @@
+class Mammal{
+    private boolean sleeping = false;
+
+    public void regulateTemperature() {
+    System.out.println("My temperature is just right now.");
+    }
+
+    public void startSleeping() {
+    sleeping = true;
+    System.out.println("ZzZz");
+    }
+    public boolean isSleeping(){
+    return sleeping;
+    }
+    public static void testStatic() {
+        System.out.println("The static method in Mammal");
+    }
+}
