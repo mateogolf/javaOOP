881ba61d632fcadebf3bc92eb19ad7b4d048f6c7
mateogolf <rodriguezml11@mail.wlu.edu>
2017-09-05 14:11:21 -0400

    Changes to Portfolio class bonus

--- Project/Portfolio.java
+++ Project/Portfolio.java
@@ -2,16 +2,41 @@ import java.util.*;
 public class Portfolio{
     private ArrayList<Project> portfolio;
     //Constructors
-    public Project(){
-        portfolio = new ArrayList<Project>;
+    public Portfolio(){
+        portfolio = new ArrayList<Project>();
     }
+    public Portfolio(Project initialProject){
+        this();
+        portfolio.add(initialProject);
+    }
+    public Portfolio(Object obj){
+        //if Project or ArrayList<Project> then add project, else return error
+        if(obj instanceof ArrayList){
+            // //check classes in arraylist,
+            // for(Object value:obj){
 
-    
-    //Name
-    //Description
-    //initialCost
-    //Elevator Pitch
-    public String elevatorPitch(){
-        return name + " ("+cost+"): " + description;
+            // }
+            // //successful
+            // portfolio = new ArrayList<Project>(Arrays.asList(obj));
+        }
+        else{
+            portfolio = new ArrayList<Project>();
+            System.out.println("The object inputted:" + obj.toString() + "is not an object, blank portfolio created");
+            
+
+        }
+    }
+    //getter and setter
+    public ArrayList<Project> getPortfolio(){
+        return portfolio;
+    }
+    public void setPortfolio(ArrayList<Project> newProjects){
+        for(Project value:newProjects){
+            portfolio.add(value);
+        }
+
+    }
+    public void addProject(Project newProj){
+        portfolio.add(newProj);
     }
 }
